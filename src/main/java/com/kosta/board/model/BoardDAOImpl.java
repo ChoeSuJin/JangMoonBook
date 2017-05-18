722bcf798a18686f835dda5be41fe80197623961
Hyeshin <amist04@naver.com>
2017-05-18 17:56:26 +0900

    Q&A 게시판

---
+++ src/main/java/com/kosta/board/model/BoardDAOImpl.java
@@ -0,0 +1,50 @@
+package com.kosta.board.model;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BoardDAOImpl implements BoardDAO {
+
+	@Inject
+	SqlSession sqlSession;
+	
+	@Override
+	public List<BoardVO> listAll() throws Exception {
+		return sqlSession.selectList("board.listAll");
+	}
+	
+	@Override
+	public void create(BoardVO vo) throws Exception {
+		sqlSession.insert("board.insert", vo);
+		
+	}
+
+	@Override
+	public BoardVO read(int bno) throws Exception {
+		return sqlSession.selectOne("board.viewBoard", bno);
+	}
+
+	@Override
+	public void increaseViewcnt(int bno) throws Exception {
+		sqlSession.update("board.viewCount", bno);
+	}
+	
+	@Override
+	public void update(BoardVO vo) throws Exception {
+		sqlSession.update("board.update", vo);	
+	}
+
+	@Override
+	public void delete(int bno) throws Exception {
+		sqlSession.delete("board.delete", bno);
+	}
+
+	
+
+
+}
